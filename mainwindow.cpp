939e34c701a88e52dc698944f092726b0cbc91dd
tcolwell95 <tcolwell95@users.noreply.github.com>
2020-12-17 16:31:55 -0500

    push as of 12.17.20 at 4:31

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "second_window.h"
+#include <QString>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    for (int i = 0; i < 20; i++){
+        ui->roof_List->addItem("item " );
+    }
 }
 
 MainWindow::~MainWindow()
@@ -14,9 +20,55 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_generateButton_pressed()
 {
     Second_window materialscreen;
     materialscreen.setModal(true);
     materialscreen.exec();
+}
+
+void MainWindow::on_addSection_clicked()
+{
+
+}
+
+void MainWindow::on_shingletype_textEdited(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_pitchNumerator_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_premiumCheckbox_stateChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_unitSquare_textEdited(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_roofHeight_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_addSection_pressed()
+{
+
+}
+
+void MainWindow::on_updateButton_pressed()
+{
+
+}
+
+void MainWindow::on_profitSlider_sliderMoved(int position)
+{
+    QString st = QString::number(position);
+    ui->profit_label->setText(st + '%');
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,7 +16,25 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_generateButton_pressed();
+
+    void on_addSection_clicked();
+
+    void on_shingletype_textEdited(const QString &arg1);
+
+    void on_pitchNumerator_valueChanged(int arg1);
+
+    void on_premiumCheckbox_stateChanged(int arg1);
+
+    void on_unitSquare_textEdited(const QString &arg1);
+
+    void on_roofHeight_valueChanged(int arg1);
+
+    void on_addSection_pressed();
+
+    void on_updateButton_pressed();
+
+    void on_profitSlider_sliderMoved(int position);
 
 private:
     Ui::MainWindow *ui;
